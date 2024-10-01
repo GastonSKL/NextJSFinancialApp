fef143031cc8a72e11deb14ea83ba177a1fdbee8
GastonSKL <gastonlskamlec@gmail.com>
2024-10-01 01:31:29 -0300

    [add][01/10/2024][financeapp] Add sign up functionality with appwrite

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -18,22 +18,53 @@ import {
 import { Input } from "@/components/ui/input";
 import CustomInput from "./CustomInput";
 import { authFormSchema } from "@/lib/utils";
-
+import { Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { signIn, signUp } from "@/lib/actions/user.actions";
 
 function AuthForm({ type }: { type: string }) {
+  const router = useRouter();
   const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const form = useForm<z.infer<typeof authFormSchema>>({
-    resolver: zodResolver(authFormSchema),
+  const formSchema = authFormSchema(type);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
     defaultValues: {
       email: "",
-      password: ""
+      password: "",
     },
   });
 
-  function onSubmit(values: z.infer<typeof authFormSchema>) {
-    console.log(values);
-  }
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    setIsLoading(true);
+
+    try {
+      if (type === "sign-up") {
+
+        const newUser = await signUp(data);
+
+        setUser(newUser);
+
+      }
+
+      // if (type === "sign-in") {
+
+      //   const response = await signIn({
+      //     email: data.email,
+      //     password: data.password
+      //   });
+
+      //   if(response) router.push("/");
+
+      // }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <section className="auth-form">
@@ -61,6 +92,77 @@ function AuthForm({ type }: { type: string }) {
         <>
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              {type === "sign-up" && (
+                <>
+                  <div className="flex gap-4 justify-between">
+                    <CustomInput
+                      control={form.control}
+                      name="firstName"
+                      label="First Name"
+                      placeholder="Enter your first name"
+                      type="text"
+                    />
+                    <CustomInput
+                      control={form.control}
+                      name="lasttName"
+                      label="Last Name"
+                      placeholder="Enter your last name"
+                      type="text"
+                    />
+                  </div>
+
+                  <CustomInput
+                    control={form.control}
+                    name="address"
+                    label="Address"
+                    placeholder="Enter your address"
+                    type="text"
+                  />
+
+                  <CustomInput
+                    control={form.control}
+                    name="city"
+                    label="City"
+                    placeholder="Enter your city name"
+                    type="text"
+                  />
+
+                  <div className="flex gap-4 justify-between">
+                    <CustomInput
+                      control={form.control}
+                      name="state"
+                      label="State"
+                      placeholder="Enter your state"
+                      type="text"
+                    />
+                    <CustomInput
+                      control={form.control}
+                      name="postalCode"
+                      label="Postal Code"
+                      placeholder="Enter your postal code"
+                      type="text"
+                    />
+                  </div>
+
+                  <div className="flex gap-4 justify-between">
+                    <CustomInput
+                      control={form.control}
+                      name="dateofbirth"
+                      label="Date of Birth"
+                      placeholder="Enter your day of birth"
+                      type="text"
+                    />
+                    <CustomInput
+                      control={form.control}
+                      name="ssn"
+                      label="SSN"
+                      placeholder="ex: 1234"
+                      type="text"
+                    />
+                  </div>
+                </>
+              )}
+
               <CustomInput
                 control={form.control}
                 name="email"
@@ -75,9 +177,39 @@ function AuthForm({ type }: { type: string }) {
                 placeholder="Enter your password"
                 type="password"
               />
-              <Button type="submit">Submit</Button>
+              <div className="flex flex-col gap-4">
+                <Button
+                  type="submit"
+                  className="form-btn w-full"
+                  disabled={isLoading}
+                >
+                  {isLoading ? (
+                    <>
+                      <Loader2 size={20} className="animate-spin" /> &nbsp;
+                      Loading...
+                    </>
+                  ) : type === "sign-in" ? (
+                    "Sign In"
+                  ) : (
+                    "Sign Up"
+                  )}
+                </Button>
+              </div>
             </form>
           </Form>
+          <footer className="flex justify-center gap-1">
+            <p className="text-14 font-normal text-gray-600">
+              {type === "sign-in"
+                ? "Don't have an account?"
+                : "Already have an acount?"}
+            </p>
+            <Link
+              href={type === "sign-in" ? "/sign-up" : "/sign-in"}
+              className="form-link"
+            >
+              {type === "sign-in" ? "Sign Up" : "Sign In"}
+            </Link>
+          </footer>
         </>
       )}
     </section>
