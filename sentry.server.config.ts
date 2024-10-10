2d3cfc48ee4f59eb232b8710b706b485df701357
GastonSKL <gastonlskamlec@gmail.com>
2024-10-09 23:22:35 -0300

    [add][09/10/2024][financeapp] set up sentry for securing the application errors for replays and more detailed info

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://fd9211f98f20deccd6d8234262b11b3b@o4508096022446080.ingest.us.sentry.io/4508096024739840",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
